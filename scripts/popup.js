34bec5aa641864ac3e42709ee23209f144baff0c
Janina <janina.huether@aol.com>
2014-08-12 23:10:06 +0200

    Hallo.

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,6 +1,7 @@
 var appIds = [],
   appIds_discount = [],
-  CHUNK_SIZE = 150;
+  XHRs = [],
+  CHUNK_SIZE = 200;
 
 var getAllApps = function() {
   $.ajax({
@@ -39,8 +40,9 @@ var getAllApps = function() {
 var getAppDetails = function() {
   var chunkCount = Math.floor(appIds.length / CHUNK_SIZE),
     appIds_chunk = 0;
-
+  var benchmark_begin, benchmark_end, benchmark = 0;
   var callback = function(response) {
+
     $.each(response, function(key, value) {
       if (value.success === true && !$.isArray(value.data)) {
         if (value.data.price_overview.discount_percent !== 0) {
@@ -51,17 +53,22 @@ var getAppDetails = function() {
     console.log(appIds_discount, appIds_discount.length);
   };
 
+  benchmark_begin = Date.now();
   for (var i = 0; i <= chunkCount; i++) {
     appIds_chunk = makeChunk(appIds);
 
-    $.ajax({
+    XHRs.push($.ajax({
       url: "http://store.steampowered.com/api/appdetails/?appids=" + appIds_chunk.toString() + "&cc=DE&l=english&filters=price_overview",
       type: "GET",
       accepts: "application/json",
       retryLimit: 3
-    })
-      .done(function(response) {
+    }));
+    /*.done(function(response) {
         callback(response);
+        var benchmark_end = Date.now();
+        console.log((benchmark_end - this.benchmark_begin)/1000);
+        benchmark += (benchmark_end - this.benchmark_begin)/1000;
+        
       })
       .error(function(xhr, textStatus, errorThrown) {
         if (textStatus == 'timeout') {
@@ -79,16 +86,23 @@ var getAppDetails = function() {
         } else {
           //handle error
         }
-      });
+      }); */
   }
-  $.ajax({
+
+
+  XHRs.push($.ajax({
     url: "http://store.steampowered.com/api/appdetails/?appids=" + appIds.toString() + "&cc=DE&l=english&filters=price_overview",
     type: "GET",
     accepts: "application/json",
     retryLimit: 3
-  })
-    .done(function(response) {
+  }));
+  /*.done(function(response) {
       callback(response);
+      var benchmark_end = Date.now();
+      console.log((benchmark_end - this.benchmark_begin)/1000);
+      benchmark += (benchmark_end - this.benchmark_begin)/1000;
+      
+      
     })
     .error(function(xhr, textStatus, errorThrown) {
       if (textStatus == 'timeout') {
@@ -105,7 +119,38 @@ var getAppDetails = function() {
       } else {
         //handle error
       }
+    }); */
+  $.each(XHRs, function(index, value) {
+    value.done(function(response) {
+      callback(response);
     });
+    value.error(function(xhr, textStatus, errorThrown) {
+      if (textStatus == 'timeout') {
+        this.tryCount++;
+        if (this.tryCount <= this.retryLimit) {
+          //try again
+          // TO DO
+          XHRs.push($.ajax(this));
+          return;
+        }
+        return;
+      }
+      if (xhr.status == 500) {
+        //handle error
+      } else {
+        //handle error
+      }
+    });
+  });
+
+  var defer = $.when.apply($, XHRs);
+  defer.done(function(){
+
+    benchmark_end = Date.now();
+    console.log((benchmark_end - benchmark_begin)/1000);
+
+  });
+
 };
 
 function makeChunk(sourceArray) {
