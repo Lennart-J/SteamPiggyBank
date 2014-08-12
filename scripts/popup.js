06ea90babc6a05c89d7f32af4890403f35f91def
LennartJ <l.jarms@hotmail.de>
2014-08-12 21:05:01 +0200

    :)

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,5 +1,6 @@
 var appIds = [],
-  appIds_discount = [];
+  appIds_discount = [],
+  CHUNK_SIZE = 150;
 
 var getAllApps = function() {
   $.ajax({
@@ -19,33 +20,36 @@ var getAllApps = function() {
 
 
 var getAppDetails = function() {
-  var chunkSize = Math.floor(appIds.length / 300),
+  var chunkCount = Math.floor(appIds.length / CHUNK_SIZE),
     appIds_chunk = 0;
 
   var callback = function(response) {
     $.each(response, function(key, value) {
-      if (value.success === true && Object.prototype.toString.call(value.data) !== '[object Array]') {
+      if (value.success === true && !$.isArray(value.data)) {
         if (value.data.price_overview.discount_percent !== 0) {
           appIds_discount.push(key);
         }
       }
     });
-    console.log(response);
-    console.log(appIds_discount);
+    console.log(appIds_discount, appIds_discount.length);
   };
 
-  for (var i = 0; i <= chunkSize/2; i++) {
+  for (var i = 0; i <= chunkCount; i++) {
     appIds_chunk = makeChunk(appIds);
 
     $.ajax({
-      url: "http://store.steampowered.com/api/appdetails/?appids=" + appIds_chunk.toString() + "&cc=DE&l=english&filters=price_overview"
+      url: "http://store.steampowered.com/api/appdetails/?appids=" + appIds_chunk.toString() + "&cc=DE&l=english&filters=price_overview",
+      type: "GET",
+      accepts: "application/json"
     })
       .done(function(response) {
         callback(response);
       });
   }
   $.ajax({
-    url: "http://store.steampowered.com/api/appdetails/?appids=" + appIds.toString() + "&cc=DE&l=english&filters=price_overview"
+    url: "http://store.steampowered.com/api/appdetails/?appids=" + appIds.toString() + "&cc=DE&l=english&filters=price_overview",
+    type: "GET",
+    accepts: "application/json"
   })
     .done(function(response) {
       callback(response);
@@ -55,7 +59,7 @@ var getAppDetails = function() {
 function makeChunk(sourceArray) {
   var chunk = [];
 
-  chunk = sourceArray.splice(0, 250);
+  chunk = sourceArray.splice(0, CHUNK_SIZE);
 
   return chunk;
 }
