c28652c5c49ec00904f8f73b4ac234e4571ac86e
LennartJ <l.jarms@hotmail.de>
2014-08-13 20:38:53 +0200

    Refactored

--- scripts/popup.js
+++ scripts/popup.js
@@ -13,152 +13,66 @@ var getAllApps = function() {
     .done(function(response) {
       $.each(response.applist.apps, function(index, value) {
         appIds.push(value.appid);
-
       });
       //console.log(response.applist.apps);
       console.log(appIds);
-    })
-    .error(function(xhr, textStatus, errorThrown) {
-      if (textStatus == 'timeout') {
-        this.tryCount++;
-        if (this.tryCount <= this.retryLimit) {
-          //try again
-          $.ajax(this);
-          return;
-        }
-        return;
-      }
-      if (xhr.status == 500) {
-        //handle error
-      } else {
-        //handle error
-      }
     });
 };
 
+var getAppDetailsAjaxCall = function(appIds) {
+  return $.ajax({
+    url: "http://store.steampowered.com/api/appdetails/?appids=" + appIds.toString() + "&cc=DE&l=english&filters=price_overview",
+    type: "GET",
+    accepts: "application/json",
+    statusCode: {
+      200: function(data, textStatus, jqXHR) {
+        $.each(data, function(key, value) {
+          if (value.success === true && !$.isArray(value.data)) {
+            if (value.data.price_overview.discount_percent !== 0) {
+              appIds_discount.push(key);
+            }
+          }
+        });
+        console.log(appIds_discount, appIds_discount.length);
+      },
+      500: function(jqXHR, textStatus, errorThrown) {
+        /*console.warn("RETRYING REQUEST");
+        setTimeout(function() {
+          getAppDetailsAjaxCall(appIds);
+        }, 5000);*/
+
+      }
+    }
+  });
+};
 
 var getAppDetails = function() {
   var chunkCount = Math.floor(appIds.length / CHUNK_SIZE),
     appIds_chunk = 0;
-  var benchmark_begin, benchmark_end, benchmark = 0;
-  var callback = function(response) {
 
-    $.each(response, function(key, value) {
-      if (value.success === true && !$.isArray(value.data)) {
-        if (value.data.price_overview.discount_percent !== 0) {
-          appIds_discount.push(key);
-        }
-      }
-    });
-    console.log(appIds_discount, appIds_discount.length);
-  };
+  var benchmark_begin,
+    benchmark_end,
+    benchmark = 0;
 
   benchmark_begin = Date.now();
+
   for (var i = 0; i <= chunkCount; i++) {
     appIds_chunk = makeChunk(appIds);
-
-    XHRs.push($.ajax({
-      url: "http://store.steampowered.com/api/appdetails/?appids=" + appIds_chunk.toString() + "&cc=DE&l=english&filters=price_overview",
-      type: "GET",
-      accepts: "application/json",
-      retryLimit: 3
-    }));
-    /*.done(function(response) {
-        callback(response);
-        var benchmark_end = Date.now();
-        console.log((benchmark_end - this.benchmark_begin)/1000);
-        benchmark += (benchmark_end - this.benchmark_begin)/1000;
-        
-      })
-      .error(function(xhr, textStatus, errorThrown) {
-        if (textStatus == 'timeout') {
-          this.tryCount++;
-          if (this.tryCount <= this.retryLimit) {
-            //try again
-            console.info("Retry_"+tryCount);
-            $.ajax(this);
-            return;
-          }
-          return;
-        }
-        if (xhr.status == 500) {
-          //handle error
-        } else {
-          //handle error
-        }
-      }); */
+    XHRs.push(getAppDetailsAjaxCall(appIds_chunk));
   }
 
-
-  XHRs.push($.ajax({
-    url: "http://store.steampowered.com/api/appdetails/?appids=" + appIds.toString() + "&cc=DE&l=english&filters=price_overview",
-    type: "GET",
-    accepts: "application/json",
-    retryLimit: 3
-  }));
-  /*.done(function(response) {
-      callback(response);
-      var benchmark_end = Date.now();
-      console.log((benchmark_end - this.benchmark_begin)/1000);
-      benchmark += (benchmark_end - this.benchmark_begin)/1000;
-      
-      
-    })
-    .error(function(xhr, textStatus, errorThrown) {
-      if (textStatus == 'timeout') {
-        this.tryCount++;
-        if (this.tryCount <= this.retryLimit) {
-          //try again
-          $.ajax(this);
-          return;
-        }
-        return;
-      }
-      if (xhr.status == 500) {
-        //handle error
-      } else {
-        //handle error
-      }
-    }); */
-  $.each(XHRs, function(index, value) {
-    value.done(function(response) {
-      callback(response);
-    });
-    value.error(function(xhr, textStatus, errorThrown) {
-      if (textStatus == 'timeout') {
-        this.tryCount++;
-        if (this.tryCount <= this.retryLimit) {
-          //try again
-          // TO DO
-          XHRs.push($.ajax(this));
-          return;
-        }
-        return;
-      }
-      if (xhr.status == 500) {
-        //handle error
-      } else {
-        //handle error
-      }
-    });
-  });
+  XHRs.push(getAppDetailsAjaxCall(appIds));
 
   var defer = $.when.apply($, XHRs);
-  defer.done(function(){
-
+  defer.done(function() {
     benchmark_end = Date.now();
-    console.log((benchmark_end - benchmark_begin)/1000);
-
+    console.log((benchmark_end - benchmark_begin) / 1000);
+    //TODO ready to continue :)
   });
-
 };
 
 function makeChunk(sourceArray) {
-  var chunk = [];
-
-  chunk = sourceArray.splice(0, CHUNK_SIZE);
-
-  return chunk;
+  return sourceArray.splice(0, CHUNK_SIZE);
 }
 
 $(document).ready(function() {
