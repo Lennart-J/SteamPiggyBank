11bd54379ceb76c490b8eb86e3a47c936f6123fd
Lennart Jarms <Lennart Jarms>
2016-08-14 21:53:16 +0200

    added jshint config file

--- scripts/lib/ga.js
+++ scripts/lib/ga.js
@@ -6,4 +6,3 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 ga('create', 'UA-55274189-1', 'auto');
 ga('set', 'checkProtocolTask', function(){});
 ga('require', 'displayfeatures');
-ga('send', 'pageview', '/popup.html');
